994011ba2171222456d79a810b7bbf10f7a96727
Mohamed Adel <mohamed.adel201568@gmail.com>
2026-01-09 10:08:04 +0000

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/mo/nutrisport/App.kt
@@ -0,0 +1,14 @@
+package com.mo.nutrisport
+
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/mo/nutrisport/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.mo.nutrisport
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
